4189a21ee387a9abc9bff0201d9254e7ae8fd09a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-27 16:52:59 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search(name)
+  def self.search(name)
     Student.find_by(name: name)
   end
 end
